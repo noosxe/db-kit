a611ec4e1eefa05829f7e720c77c554b632f17d8
Levon Kirakosyan <noosx.e@gmail.com>
2013-08-06 18:04:14 +0400

    object build checks for valid keys used object build now installing getter/setters readOnly properties allowed nested objects generation done

--- test/index.js
+++ test/index.js
@@ -6,20 +6,43 @@
       debug: true
   });
 
-  var Test = kit.define('test', {
+  var User = kit.define('user', {
     email: {
-      type: kit.types.STRING
+      type: kit.types.STRING,
+      null: false,
+      readOnly: true
     },
     password: {
-      type: kit.types.STRING
+      type: kit.types.STRING,
+      null: false
     },
     firstName: kit.types.STRING,
     lastName: kit.types.STRING
   });
 
-  var t = Test.build();
+  var Project = kit.define('project', {
+    name: {
+      type: kit.types.STRING,
+      null: false
+    },
+    author: {
+      type: kit.types.INT,
+      reference: {
+        entity: User,
+        field: 'id'
+      },
+      readOnly:true
+    }
+  });
 
-  console.log(Test);
-  console.log(t);
+  var project = Project.build({
+    name: 'My project',
+    author: {
+      email: 'levon@toort.net',
+      password: 'bacon'
+    }
+  });
+
+  console.log(project.getValues());
 
 }());
