a8d8836c9fc970499a1d1227c73690aa5de086ca
Levon Kirakosyan <noosx.e@gmail.com>
2014-05-23 16:18:15 +0400

    work in progress

--- test/index.js
+++ test/index.js
@@ -1,46 +1,16 @@
-(function() {
-  "use strict";
+"use strict";
 
-  var util = require('util')
-    , i = function(obj) {
-        console.log(util.inspect(obj, { showHidden: true, depth: null, colors: true }));
-      };
+var path = require('path');
+var schemasDir = path.join(__dirname, "schemas");
+var Kit = require("../index.js");
 
-  //require('./postgresql').kit.run();
+var kit = new Kit({
+	adapter: Kit.adapters.MYSQL,
+	connString: "mysql://root:@localhost:3306/test"
+});
 
-  var Kit = require('../index.js');
-  var kit = new Kit('kit', 'postgres', {
-    native: true,
-    debug: true
-  });
-
-  var Project = kit.define('Project', {
-    make: { type: kit.types.STRING, required: true },
-    model: { type: kit.types.STRING, required: true },
-    hp: { type: kit.types.INT, required: true },
-    maxSpeed: { type: kit.types.INT, required: true },
-    productionDate: { type: kit.types.DATE, required: true }
-  }, {
-    timestamps: true
-  });
-
-  var Worker = kit.define('Worker', {
-    email: { type: kit.types.STRING, required: true, unique: true, readOnly: true,
-      match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/ },
-    password: { type: kit.types.STRING, required: true, hidden: true, minLength: 8 },
-    firstName: kit.types.STRING,
-    lastName: kit.types.STRING,
-    birthDate: { type: kit.types.DATE, required: true },
-    gender: { type: kit.types.enum('gender', ['male', 'female']), required: true },
-    phoneNumbers: { type: [kit.types.STRING] }
-  }, {
-    timestamps: true
-  });
-
-  Project.as('projects').field('id').manyToMany(Worker.as('workers'));
-
-  kit.sync().on('done', function() {
-    console.log('synced');
-  });
-
-}());
+kit.query({ text: "SELECT ?+? AS result", values: [4, 5] }).on('error', function(err) {
+	console.log(err);
+}).on('done', function(result) {
+	console.log(result);
+});
